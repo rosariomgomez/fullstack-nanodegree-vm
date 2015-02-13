34af3b36a1f595ac3aba66a272dc024923731c1f
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-13 14:43:46 -0800

    added table definitions

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,19 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players (
+	id serial PRIMARY KEY,
+	name text
+);
 
+CREATE TABLE matches (
+	id serial PRIMARY KEY,
+	player1 integer REFERENCES players(id),
+	player2 integer REFERENCES players(id)
+);
+
+CREATE TABLE wins (
+	match integer REFERENCES matches(id),
+	winner integer REFERENCES players(id),
+	PRIMARY KEY (match, winner)
+);
